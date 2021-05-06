74d8a282642f2808ebde6b162260b87855fd990b
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-05-05 19:50:44 -0500

    :sparkles: [ADD] Firmando y verificando nuestro JWT -> exec

--- jwt-utilities/index.js
+++ jwt-utilities/index.js
@@ -2,6 +2,8 @@ const jwt = require("jsonwebtoken");
 
 const [, , option, secret, nameOrToken] = process.argv;
 
+console.log('=>', process.argv );
+
 if (!option || !secret || !nameOrToken) {
   return console.log("Missing arguments");
 }
@@ -21,3 +23,9 @@ if (option == "sign") {
 } else {
   console.log('Option needs to be "sign" or "verify"');
 }
+
+//call genera token
+//node jwt-utilities/index.js sign secret osvaldo
+
+//call verifica token
+//node jwt-utilities/index.js verify secret eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJvc3ZhbGRvIiwiaWF0IjoxNjIwMjYxODI5fQ.pKQWNCvleXzG-FpC3Ee1Mea1PUrHFfHrbxwWPMIa2hQ
